494261e1fac8dd46941d79d30ec0dc255902ea26
Mohammed Malik <malikmohammed2067@gmail.com>
2025-09-03 10:16:53 -0700

    Add files via upload

---
+++ Recurssion/DecreasingOrder.java
@@ -0,0 +1,21 @@
+public class DecreasingOrder{
+    //recurssion
+    public static void printDec(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+
+    } 
+
+
+
+
+
+    public static void main(String[] args) {
+        printDec(10);
+        
+    }
+}
